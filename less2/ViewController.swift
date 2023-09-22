8c4e8f4f375d2ebf586821869e6a2b4577017cb5
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-09-22 17:10:20 +0500

    1. setup UI 2. work with logic

--- less2/ViewController.swift
+++ less2/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var greetingLabel: UILabel!
 
+    @IBOutlet weak var greetingButtom: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden = true
+        greetingButtom.layer.cornerRadius = 10
+        greetingButtom.setTitle("Show Greeting", for: .normal)
     }
 
-
+    @IBAction func greetingButtonPressed() {
+        greetingLabel.isHidden.toggle()
+        
+//        if greetingLabel.isHidden {
+//            greetingButtom.setTitle("Show Greeting", for: .normal)
+//        } else {
+//            greetingButtom.setTitle("Hide greeting", for: .normal)
+//        }
+        greetingButtom.setTitle(
+            greetingLabel.isHidden
+            ? "Show Greeting"
+            : "Hide greeting",
+            for: .normal
+        )
+    }
+    
 }
 
